00c8107af4aa370f8931eb8b206123c4f3d501f0
ARMANOD <stickarmand11@gmail.com>
2023-04-05 15:13:06 -0600

    added coomets

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        //my second change.
+        //my tercer change.
         [Key]
         public int CartItemId { get; set; }
 
